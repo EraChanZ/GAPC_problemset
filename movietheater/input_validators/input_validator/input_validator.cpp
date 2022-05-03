8b57c698f72af135431f8b94548dc48f4252f0ff
Ilya Sashcheka <sas4eka@gmail.com>
2022-05-03 19:35:57 +0200

    Movie Theater: raise the limits to 500

--- movietheater/input_validators/input_validator/input_validator.cpp
+++ movietheater/input_validators/input_validator/input_validator.cpp
@@ -2,9 +2,9 @@
 
 int main(int argc, char** argv) {
     InputValidator v(argc, argv);
-    int n = v.read_integer("n", 3, 100);
+    int n = v.read_integer("n", 3, 500);
     v.space();
-    int m = v.read_integer("m", 3, 100);
+    int m = v.read_integer("m", 3, 500);
     v.newline();
 
     for (int i = 0; i < n; i++) {
