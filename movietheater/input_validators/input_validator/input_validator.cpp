5bba99b1e5d0c663c6bc8090ba11d50a212ce0f4
Ilya Sashcheka <sas4eka@gmail.com>
2022-05-03 23:50:09 +0200

    Movie Theater: improve input validation
    Validate the content, not only the input size.

--- movietheater/input_validators/input_validator/input_validator.cpp
+++ movietheater/input_validators/input_validator/input_validator.cpp
@@ -1,4 +1,5 @@
 #include "validation.h"
+#include <vector>
 
 int main(int argc, char** argv) {
     InputValidator v(argc, argv);
@@ -7,9 +8,49 @@ int main(int argc, char** argv) {
     int m = v.read_integer("m", 3, 500);
     v.newline();
 
+    std::vector<std::string> grid(n);
     for (int i = 0; i < n; i++) {
-        v.read_string("line", m, m);
+        grid[i] = v.read_string("line", m, m);
         v.newline();
+    }
+
+    bool haveE = false;
+    bool haveX = false;
+    bool haveS = false;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            bool isSide = i == 0 || i == n - 1 || j == 0 || j == m-1; 
+            if (grid[i][j] == 'E') {
+                haveE = true;
+                if (!isSide) {
+                    v.WA("Input contains E not on the side");
+                }
+            } else if (grid[i][j] == 'X') {
+                haveX = true;
+                if (!isSide) {
+                    v.WA("Input contains X not on the side");
+                }
+            } else if (grid[i][j] == 'S') {
+                haveS = true;
+                if (isSide) {
+                    v.WA("Input contains S on the side");
+                }
+            } else if (grid[i][j] == '.') {
+
+            } else {
+                v.WA("Input contains invalid character");
+            }
+        }
+    }
+
+    if (!haveE) {
+        v.WA("Input doesn't contain E");
+    }
+    if (!haveX) {
+        v.WA("Input doesn't contain X");
+    }
+    if (!haveS) {
+        v.WA("Input doesn't contain S");
     }
 
     return 0;
