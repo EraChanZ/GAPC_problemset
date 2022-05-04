47b58c3c11fa5b6df776cb70ed3fae07e6eaa006
Ilya Sashcheka <sas4eka@gmail.com>
2022-05-04 17:21:12 +0200

    Dice Game: finalize the problem
    - Rework problem statement
    - Make sure that samples nicely fit in the PDF
    - Bump the number of players ans sides to 2 to avoid uncertainty

--- dicegame/input_validators/input_validator/input_validator.cpp
+++ dicegame/input_validators/input_validator/input_validator.cpp
@@ -2,9 +2,9 @@
 
 int main(int argc, char** argv) {
     InputValidator v(argc, argv);
-    int n = v.read_integer("n", 1, 1000);
+    int n = v.read_integer("n", 2, 1000);
     v.space();
-    int m = v.read_integer("m", 1, 1000);
+    int m = v.read_integer("m", 2, 1000);
     v.newline();
     return 0;
 }
